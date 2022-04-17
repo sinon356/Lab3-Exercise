b22faa1be302610b5f45ac896e90b4663ea03434
Ismail Izzuddin <Ismail Izzuddin@DESKTOP-U5P08U4>
2022-04-18 01:39:32 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lbWeight = New System.Windows.Forms.ListBox()
+        Me.lbZone = New System.Windows.Forms.ListBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(38, 56)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Weight"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(38, 93)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(113, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "(Not to exceed)"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(269, 26)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(43, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Zone"
+        '
+        'lbWeight
+        '
+        Me.lbWeight.FormattingEnabled = True
+        Me.lbWeight.ItemHeight = 20
+        Me.lbWeight.Items.AddRange(New Object() {"1 lb", "3 lb", "5 lb", "10 lb", ">10 lb"})
+        Me.lbWeight.Location = New System.Drawing.Point(49, 126)
+        Me.lbWeight.Name = "lbWeight"
+        Me.lbWeight.Size = New System.Drawing.Size(102, 124)
+        Me.lbWeight.TabIndex = 3
+        '
+        'lbZone
+        '
+        Me.lbZone.FormattingEnabled = True
+        Me.lbZone.ItemHeight = 20
+        Me.lbZone.Items.AddRange(New Object() {"A", "B", "C", "D"})
+        Me.lbZone.Location = New System.Drawing.Point(269, 93)
+        Me.lbZone.Name = "lbZone"
+        Me.lbZone.Size = New System.Drawing.Size(115, 164)
+        Me.lbZone.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(202, 307)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(73, 20)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Charges : "
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.Location = New System.Drawing.Point(293, 306)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(113, 32)
+        Me.TextBox1.TabIndex = 6
+        '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(293, 376)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(113, 30)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "Look Up"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(436, 424)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.lbZone)
+        Me.Controls.Add(Me.lbWeight)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Shipping Rate Lookup"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lbWeight As ListBox
+    Friend WithEvents lbZone As ListBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox1 As Label
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim strcharges(4, 3) As String
+        strcharges(0, 0) = "1.00"
+        strcharges(0, 1) = "1.50"
+        strcharges(0, 2) = "1.65"
+        strcharges(0, 3) = "1.85"
+        strcharges(1, 0) = "1.58"
+        strcharges(1, 1) = "2.00"
+        strcharges(1, 2) = "2.40"
+        strcharges(1, 3) = "3.05"
+        strcharges(2, 0) = "1.71"
+        strcharges(2, 1) = "2.52"
+        strcharges(2, 2) = "3.10"
+        strcharges(2, 3) = "4.00"
+        strcharges(3, 0) = "2.04"
+        strcharges(3, 1) = "3.12"
+        strcharges(3, 2) = "4.00"
+        strcharges(3, 3) = "5.01"
+        strcharges(4, 0) = "2.52"
+        strcharges(4, 1) = "3.75"
+        strcharges(4, 2) = "5.10"
+        strcharges(4, 2) = "7.25"
+
+        TextBox1.Text = strcharges(lbWeight.SelectedIndex, lbZone.SelectedIndex)
+    End Sub
+End Class
